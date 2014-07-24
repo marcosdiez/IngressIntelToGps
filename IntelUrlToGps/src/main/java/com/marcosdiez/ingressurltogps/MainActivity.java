211eec44a979930b60ffe436b1ccd59bbcc3d551
Marcos Diez <marcos@unitron.com.br>
2014-07-24 17:13:01 +0000

    now we can use YandexMaps if it exists

--- IntelUrlToGps/src/main/java/com/marcosdiez/ingressurltogps/MainActivity.java
+++ IntelUrlToGps/src/main/java/com/marcosdiez/ingressurltogps/MainActivity.java
@@ -1,7 +1,10 @@
 package com.marcosdiez.ingressurltogps;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +12,33 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     final static String TAG = "Intel2GPS";
+    public static ParsedIntelUrl theData=null;
+    public static Activity theActivity=null;
+
+    public static void showExportDialog(final Activity theActivity) {
+        // final Activity activityCopy = theActivity;
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(theActivity);
+        alertDialog.setMessage(R.string.choose_your_maps_app)
+                .setPositiveButton(R.string.google_maps, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        String url = buildGoogleMapsUrl(theData.getLongitude(), theData.getLatitude());
+                        Log.d(TAG,url);
+                        openUrl(url, theActivity);
+                        theActivity.finish();
+                    }
+                })
+                .setNegativeButton(R.string.yandex_maps, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        String url = buildYandexMapsUrl(theData.getLongitude(), theData.getLatitude());
+                        Log.d(TAG,url);
+                        openUrl(url, theActivity);
+                        theActivity.finish();
+
+                    }
+                });
+
+        alertDialog.create().show();
+    }
 
 
     @Override
@@ -20,8 +50,11 @@ public class MainActivity extends Activity {
         // -23.545751,-46.66941
         //openGpsUrl("-23.545751" , "-46.66941");
         // ParsedIntelUrlTest.doTests();
+
         processIntent();
-        finish();
+        //
+
+
     }
 
     private void processIntent() {
@@ -38,17 +71,58 @@ public class MainActivity extends Activity {
         Log.d(TAG, "Received URL:" + data);
         ParsedIntelUrl parsedData = new ParsedIntelUrl(data);
         openGpsUrl(parsedData);
+
     }
 
-    void openGpsUrl(ParsedIntelUrl theData){
-        openGpsUrl(theData.getLongitude(), theData.getLatitude());
+//
+
+    void openGpsUrl(ParsedIntelUrl theDataX){
+
+    Log.d(TAG , "ru.yandex.yandexmaps: " + (appInstalledOrNot("ru.yandex.yandexmaps") ? "true" : "false") );
+
+        if(appInstalledOrNot("ru.yandex.yandexmaps")) {
+            MainActivity.theData=theDataX;
+            MainActivity.theActivity=this;
+            showExportDialog(this); // Yandex requires a different intent
+        }else {
+            // google maps
+            String url = buildGoogleMapsUrl(theDataX.getLongitude(), theDataX.getLatitude());
+            openUrl(url, this);
+            finish();
+        }
     }
 
-    void openGpsUrl(String longitude, String latitude){
+    static String buildGoogleMapsUrl(String longitude, String latitude){
+        // yes, longitude and latitude are swapped :(
         String theURL = "http://maps.google.com/maps?daddr=" + longitude + "," + latitude;
+        return theURL;
+    }
+
+    static String buildYandexMapsUrl(String longitude, String latitude){
+        // yes, longitude and latitude are swapped :(
+        String theURL = "http://maps.yandex.com/?ll=" + latitude + "," + longitude;
+        return theURL;
+    }
+
+
+    static void openUrl(String theURL, Activity theActivity){
         Log.d(TAG, "Opening location:" + theURL);
         final Intent intent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse(theURL));
-        startActivity(intent);
+        theActivity.startActivity(intent);
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed = false;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed ;
     }
 }
+
